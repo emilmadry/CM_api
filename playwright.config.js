f163719becdd2040e269bcc1ee22b294b267b327
emilmadry <redvarg91@gmail.com>
2025-11-03 16:43:12 +0100

    prettier added and applied

--- playwright.config.js
+++ playwright.config.js
@@ -1,19 +1,19 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,25 +23,25 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://reqres.in/',
+    baseURL: "https://reqres.in/",
 
     extraHTTPHeaders: {
-      'x-api-key': process.env.API_KEY ?? '',
+      "x-api-key": process.env.API_KEY ?? "",
     },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     // {
@@ -82,4 +82,3 @@ export default defineConfig({
   //   reuseExistingServer: !process.env.CI,
   // },
 });
-
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,31 +1,26 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('check get endpoint', async ({ request }) => {
-
-  const response = await request.get('/api/users/2');
+test("check get endpoint", async ({ request }) => {
+  const response = await request.get("/api/users/2");
   expect(response.status()).toBe(200);
 
-  expect(await response.text()).toContain('janet.weaver@reqres.in');
-
+  expect(await response.text()).toContain("janet.weaver@reqres.in");
 });
 
-
-test('check post endpoint', async ({ request }) => {
-
-  const response = await request.post('/api/users',
+test("check post endpoint", async ({ request }) => {
+  const response = await request.post(
+    "/api/users",
 
     {
       data: {
-        name: 'John Doe',
-        job: 'Developer'
-      }
-    }
-
+        name: "John Doe",
+        job: "Developer",
+      },
+    },
   );
   expect(response.status()).toBe(201);
 
   // console.log(await response.text());
-  expect(await response.text()).toContain('John Doe');
-
+  expect(await response.text()).toContain("John Doe");
 });
